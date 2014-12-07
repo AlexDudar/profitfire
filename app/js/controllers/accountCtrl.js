885fe1193d58d7eda2ce395670e2f7b2dd2da570
Alexandr Dudar <alexandr.dudar@gmail.com>
2014-12-07 23:03:29 +0100

    fixed filter, added month navigation

--- app/js/controllers/accountCtrl.js
+++ app/js/controllers/accountCtrl.js
@@ -22,17 +22,6 @@ angular.module('ProfitApp.AccountCtrl', [])
 
     console.log($scope.currentUser);
 
-    var sum = _.reduce([$scope.expenses], function(sum, num) {
-      return sum + num;
-    });
-
-    console.log(sum);
-
-
-//    $scope.myitems = _.where($scope.expenses, {'amount': 12});
-
-//    $scope.myitems = _.findIndex($scope.expenses, { 'amount': 12 });
-//    console.log($scope.myitems);
 
     $scope.getDate = {
       current: new Date()
@@ -90,53 +79,8 @@ angular.module('ProfitApp.AccountCtrl', [])
       $scope.userRoot.$set({totals: $scope.totalsum});
     };
 
-    //console.log('new total');
-    //console.log($scope.ttal);
 
-    //Object.keys($scope.userRoot).forEach(function(key) {
-    //  return $scope.userRoot[key]
-    //})
-
-    //$scope.userRoot.$on("loaded", function(data) {
-    //  console.log(data);
-    //
-    //  var keys = [];
-    //
-    //  //for(var k in data) {
-    //  //  if (data.hasOwnProperty(k)){
-    //  //    keys.push(k);
-    //  //  }
-    //  //}
-    //
-    //  for(var k in data) {
-    //    keys.push(k);
-    //  }
-    //  console.log(keys[0]);
-    //
-    //  //Object.keys(data).forEach(function(key) {
-    //  //  console.log(data[key].totalSum);
-    //  //  return data[key];
-    //  //
-    //  //})
-    //});
-
-
-
-
-
-    //console.log(Object.keys($scope.userRoot)[1]);
-
-
-
-
-
-      $scope.editorEnabled = false;
-
-    $scope.enableEditor = function(id) {
-//      $scope.editorEnabled = true;
-//      $scope.editorEnabled = $scope.expenses.indexOf(id);
-   //   $scope.expenseCopy = angular.copy(id);    //dont need
-    };
+    $scope.editorEnabled = false;
 
     $scope.save = function(id) {
      // var nameRef = syncData(['users', $scope.auth.user.uid + '/expenses/' + id]);
@@ -144,78 +88,23 @@ angular.module('ProfitApp.AccountCtrl', [])
       $scope.editorEnabled = false;
     };
 
-      $scope.disableEditor = function(id) {
-//        $scope.expenseCopy[$scope.editorEnabled] = false;
-//        $scope.amount = 'hui';
-//        $scope.editorEnabled = false;
-      };
 
 
     $scope.removeExpense = function(id){
       $scope.expenses.$remove(id);
     };
 
+    $scope.currentMonth = d.getMonth() + 1;
 
-
-//    $scope.$watch('expenses', function() {
-//      var Total = 0;
-//
-//      $scope.expenses.forEach(function(expense) {
-////        Total += expense.getTotal();
-//        Total += expense.amount;
-//      });
-//
-//    }, true);
-
-//    $scope.total = function() {
-//      var total = 0;
-//      angular.forEach($scope.expenses, function(expense) {
-//        console.log($scope.expenses.amount);
-//        //total += expense.amount;
-//
-//
-//      });
-//
-//      return total;
-//    };
-
-  /*------------ DATE FILTER -------------*/
-
-    var d = new Date();
-
-    var curr_date = d.getDate();
-    var curr_month = d.getMonth()+1;
-    var curr_year = d.getFullYear();
-    //console.log(curr_month);
-
-
-    $scope.dateToday = Date.parse(curr_month + "/" + curr_date + "/" + curr_year);
-    $scope.dateRange = "";
-//    $scope.dateToday = d;
-
-    $scope.dataModels = [{age:5,name:'John Lennon',eventDate:"1380524400000"},
-      {age:12,name:'Nick Young',eventDate:"1406667600000"},
-      {age:10,name:'Mike Johnson',eventDate:"1374044400000"},
-      {age:15,name:'Lisa Leslie',eventDate:"1335942000000"}
-    ];
-    $scope.dateFilter = function(column) {
-
-
-//      $scope.dateFilter = function(column) {
-        if(column === 'today') {
-          $scope.dateRange = curr_month;
-
-        } else if (column === 'pastWeek') {
-          $scope.dateRange = curr_date;
-        } else if (column === 'pastMonth') {
-          $scope.dateRange = '5';
-        } else if (column === 'future') {
-          //need logic
-        } else {
-          $scope.dateRange = "";
-        }
-//      }
+    $scope.nextMonth = function (){
+      $scope.currentMonth += 1;
     };
+
+    $scope.previousMonth = function (){
+      $scope.currentMonth -= 1;
+    };
+
+
 
 
   /*------------ PROFILE -------------*/
